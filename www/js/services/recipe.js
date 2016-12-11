eb896153255f1e4a5c5210a902aa5a10df7b414e
schmm2 <tinsu3@gmail.com>
2016-12-11 21:35:39 +0100

    add malaria recipe

--- www/js/services/recipe.js
+++ www/js/services/recipe.js
@@ -4,13 +4,23 @@ angular.module('services.recipe', [])
       {
         id: 0,
         titel: 'Ritalin',
+        against: "Müdigkeit",
         datum: '1.1.2016',
         arzt: 'Dr. Aklin',
         img: '/img/ritalin.jpg'
       },
       {
         id: 1,
+        titel: 'Malarone',
+        against: "Malaria",
+        datum: '1.1.2016',
+        arzt: 'Dr. Aklin',
+        img: '/img/malarone.jpg'
+      },
+      {
+        id: 2,
         titel: 'Zeller Schlaf',
+        against: "Schlafstörung",
         datum: '1.1.2016',
         arzt: 'Dr. Aklin',
         bezogen: '2.1.2016',
